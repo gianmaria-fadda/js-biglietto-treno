231862aa266594e0f9a3187487a836180aa1a033
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-07-12 10:21:57 +0200

    Aggiunto il tofixed

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ if (age < 18) {
 
     const finalPrice = price - discount
 
-    alert('|PREZZO RIDOTTO| il prezzo del biglietto è €' + finalPrice)
+    alert('|PREZZO RIDOTTO| il prezzo del biglietto è €' + finalPrice.toFixed(2))
 }
 else if (age > 65) {
     console.log('Over 65');
@@ -26,12 +26,12 @@ else if (age > 65) {
 
     const finalPrice = price - discount
 
-    alert('|PREZZO OVER 65| il prezzo del biglietto è €' + finalPrice)
+    alert('|PREZZO OVER 65| il prezzo del biglietto è €' + finalPrice.toFixed(2))
 }
 else {
     console.log('Paga prezzo Pieno');
     const price = km * 0.21
     console.log('price', price, typeof price)
 
-    alert('il prezzo del biglietto è €' + price)
+    alert('il prezzo del biglietto è €' + price.toFixed(2))
 }   
