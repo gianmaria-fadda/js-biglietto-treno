c35fedbd343485f810542fb21bd60ec835a6db58
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-07-12 10:16:05 +0200

    Completato esercizio (manca formatto numeri)

--- js/script.js
+++ js/script.js
@@ -1,21 +1,37 @@
-let age = '17'
+const km = prompt('Quanti chilometri vuoi percorrere?')
+console.log('km', km, typeof km)
+
+const age = prompt('Quanti anni hai?')
+console.log('age', age, typeof age)
 
 if (age < 18) {
     console.log('Minorenne');
-}
-else if (age == 18) {
-    console.log('Maggiorenne');
-}
-else if (age == 65) {
-    console.log('Over');
+    const price = km * 0.21
+    console.log('price', price, typeof price)
+
+    const discount = (price / 100) * 20
+    console.log('discount', discount, typeof discount)
+
+    const finalPrice = price - discount
+
+    alert('|PREZZO RIDOTTO| il prezzo del biglietto è €' + finalPrice)
 }
 else if (age > 65) {
-    console.log('Over');
-}
-else if (age > 18) {
-    console.log('Maggiorenne');
+    console.log('Over 65');
+    const price = km * 0.21
+    console.log('price', price, typeof price)
+
+    const discount = (price / 100) * 40
+    console.log('discount', discount, typeof discount)
+
+    const finalPrice = price - discount
+
+    alert('|PREZZO OVER 65| il prezzo del biglietto è €' + finalPrice)
 }
 else {
-    console.log('Non Corretto');
-}
-    
+    console.log('Paga prezzo Pieno');
+    const price = km * 0.21
+    console.log('price', price, typeof price)
+
+    alert('il prezzo del biglietto è €' + price)
+}   
